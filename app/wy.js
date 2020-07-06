676bd1f2a5395c90e46955c23cdb655dc4ecc0dc
tianyajiejie <59963945+tianyajiejie@users.noreply.github.com>
2020-07-06 11:43:29 +0800

    Create wy.js

---
+++ app/wy.js
@@ -0,0 +1,12 @@
+// second engine DM
+search_engine = '';
+search_engine += '<label class="checkbox-inline"><input type="radio" name="ops_engine" id="engine1" value="https://m.mijisou.com/?q=" checked>秘迹搜索</label>';
+search_engine += '<label class="checkbox-inline"><input type="radio" name="ops_engine" id="engine2" value="https://m.baidu.com/s?wd=">百度一下</label>';
+search_engine += '<label class="checkbox-inline"><input type="radio" name="ops_engine" id="engine3" value="https://wuyayy.com/search.php?searchword=">乌鸦电影</label>';
+
+// second box DM
+search_box = '';
+search_box += '<div class="box"><a href="https://wuyayy.com/"></a><p><img class="icon" src="home_player.png" alt=""></p><p class="url">乌鸦影院</p></div>';
+search_box += '<div class="box"><a href="https://m.23wxc.com/"></a><p><img class="icon" src="home_book.png" alt=""></p><p class="url">顶点小说</p></div>';
+search_box += '<div class="box"><a href="https://translate.google.cn"></a><p><img class="icon" src="home_translate.png" alt=""></p><p class="url">谷歌翻译</p></div>';
+search_box += '<div class="box"><a href="https://movie.douban.com/"></a><p><img class="icon" src="home_mark.png" alt=""></p><p class="url">豆瓣影评</p></div>';
